cf7b768045a0a46e2cc5f44ad55bf16701f63c7f
Wentai Du <wentai@cs.ualberta.ca>
2021-09-08 17:22:32 -0600

    update

--- 1.py
+++ 1.py
@@ -1,6 +1,11 @@
 import requests
 
-#print(requests.__version__)
+print(requests.__version__)
+
+
 url="https://www.google.com"
 resp=requests.get(url)
-#print(resp)
+print(resp)
+
+pythonUrl=requests.get("https://github.com/WentaiDu/CMPUT404LAB/blob/main/1.py")
+print(pythonUrl)
